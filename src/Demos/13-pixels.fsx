be6a48875979adb36ebd48ce78960b7c29b17eca
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2015-03-09 14:44:17 +0200

    update implementation

--- src/Demos/13-pixels.fsx
+++ src/Demos/13-pixels.fsx
@@ -9,12 +9,12 @@ open Nessos.Streams
 let path = __SOURCE_DIRECTORY__ + "/../../data/train.csv"
 
 type Point = int []
-type Distance = Point -> Point -> int
-type TrainingSet = (int * Point) []
-type Classifier = TrainingSet -> Point -> int
+type Distance = Point -> Point -> int64
+type TrainingPoint = int * Point // classification x point
+type Classifier = TrainingPoint [] -> Point -> int
 
 // read using streams
-let data : TrainingSet =
+let data : TrainingPoint [] =
     File.ReadAllLines path
     |> Stream.ofArray
     |> Stream.skip 1
@@ -24,15 +24,15 @@ let data : TrainingSet =
     |> Stream.toArray
 
 // separating training & validation set
-let training : TrainingSet = data.[..39999]
-let validation : TrainingSet = data.[40000..]
+let training : TrainingPoint [] = data.[..39999]
+let validation : TrainingPoint [] = data.[40000..]
  
 // l^2 distance 
-let l2 : Distance = Array.fold2 (fun acc x y -> acc + pown (x-y) 2) 0
+let l2 : Distance = Array.fold2 (fun acc x y -> acc + int64 (pown (x-y) 2)) 0L
 
 // single-threaded, stream-based k-nearest neighbour classifier
 let knn (d : Distance) (k : int) : Classifier =
-    fun (training : TrainingSet) (point : Point) ->
+    fun (training : TrainingPoint []) (point : Point) ->
         training
         |> Stream.ofArray
         |> Stream.sortBy (fun ex -> d (snd ex) point)
@@ -47,7 +47,7 @@ let classifier = knn l2 10
 
 
 // local multicore evaluation
-let evaluateLocalMulticore () =
+let evaluateLocalMulticore (classifier : Classifier) (training : TrainingPoint []) (validation : TrainingPoint []) =
     ParStream.ofArray validation
     |> ParStream.map(fun (expected,point) -> expected, classifier training point)
     |> ParStream.map(fun (expected,prediction) -> if expected = prediction then 1. else 0.)
@@ -57,7 +57,7 @@ let evaluateLocalMulticore () =
 #time
 // Performance (Quad core i7 CPU)
 // Real: 00:01:25.783, CPU: 00:10:51.647, GC gen0: 153, gen1: 73, gen2: 43
-evaluateLocalMulticore()
+evaluateLocalMulticore classifier training validation
 
 
 //
@@ -75,39 +75,21 @@ let cluster = Runtime.GetHandle(config)
 // Save training set as cloud reference in Azure store, returning a typed reference to data
 let trainingRef = cluster.DefaultStoreClient.CloudSequence.New training
 
-//// work around store performance bug using this monstrosity: (see also https://github.com/mbraceproject/MBrace.Azure/issues/26)
-//let createCloudSequence(ts : seq<'T>) =
-//    let serialize (config : Store.CloudFileStoreConfiguration) = async {
-//        let localPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName())
-//        let fs = File.OpenWrite localPath 
-//        let _ = config.Serializer.SeqSerialize(fs, ts, false)
-//        use fs = File.OpenRead localPath
-//        let storePath = config.FileStore.GetRandomFilePath(config.DefaultDirectory)
-//        do! config.FileStore.OfStream(fs, storePath)
-//        return storePath
-//    }
-//
-//    cloud {
-//        let! config = Continuation.Cloud.GetResource<Store.CloudFileStoreConfiguration> ()
-//        let! store = Cloud.OfAsync <| serialize config
-//        return! CloudSequence.Parse<'T>(store, serializer = config.Serializer)
-//    }
-//
-//let trainingRef = training |> createCloudSequence |> cluster.RunLocal
-
-let evaluateDistributed (inputs : (int * Point) []) = cloud {
-    let evaluateSingleThreaded (inputs : (int * Point) []) = cloud {
-        let! _ = trainingRef.Cache() // cache to local memory for future use
+let evaluateDistributed (classifier : Classifier) (training : CloudSequence<TrainingPoint>) (validation : TrainingPoint []) = cloud {
+    let evaluateSingleThreaded (validation : TrainingPoint []) = local {
+        let! _ = trainingRef.PopulateCache() // cache to local memory for future use
         let! training = trainingRef.ToArray()
         return
-            inputs
+            validation
             |> Stream.ofArray
             |> Stream.map (fun (expected,point) -> if expected = classifier training point then 1 else 0)
             |> Stream.sum
     }
 
-    let! successful = inputs |> Distributed.reduceCombine evaluateSingleThreaded (Cloud.lift Array.sum)
-    return float successful / float inputs.Length
+    let! successful = validation |> Distributed.reduceCombine evaluateSingleThreaded (Local.lift Array.sum)
+    return float successful / float validation.Length
 }
 
-let job = validation |> evaluateDistributed |> cluster.CreateProcess
+let job = evaluateDistributed classifier trainingRef validation |> cluster.CreateProcess
+
+job.AwaitResult()
